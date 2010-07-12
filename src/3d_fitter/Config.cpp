720044516f877116142f1b38f51ee5376a4030ca
stw78ye@BIOPHYSIK.LOCAL <stw78ye@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2010-07-12 07:20:33 +0000

    Enabled both sqrt-sigma and parabol-sigma
    git-svn-id: file:///scratch/steve/svn/rapidSTORM/branches/rapid2storm-1.5@359 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

--- src/3d_fitter/Config.cpp
+++ src/3d_fitter/Config.cpp
@@ -16,7 +16,8 @@ Config::Config()
 : MarquardtConfig("3DFitter", "3D cylinder lens fit"),
   z_distance("ZDistance", "Distance between X and Y foci"),
   defocus_constant_x("XDefocusConstant", "Speed of PSF std. dev. growth in X"),
-  defocus_constant_y("YDefocusConstant", "Speed of PSF std. dev. growth in Y")
+  defocus_constant_y("YDefocusConstant", "Speed of PSF std. dev. growth in Y"),
+  holtzer_psf("HoltzerPSF", "Use Holtzerian PSF", false)
 {
 }
 
@@ -32,6 +33,7 @@ void Config::registerNamedEntries()
     push_back(z_distance);
     push_back(defocus_constant_x);
     push_back(defocus_constant_y);
+    push_back(holtzer_psf);
     fitter::residue_analysis::Config::registerNamedEntries(*this);
 }
 
--- src/3d_fitter/Exponential3D_ParameterHelper.hh
+++ src/3d_fitter/Exponential3D_ParameterHelper.hh
@@ -15,8 +15,8 @@ bool ParameterHelper<Special>::prepare(
 ) {
     Base::extract( v, c );
     Eigen::Matrix<double,Kernels,1> z, z0x, z0y, v0x, v0y;
-    Base::template extract_param<BestVarianceX>( v, c, v0x );
-    Base::template extract_param<BestVarianceY>( v, c, v0y );
+    Base::template extract_param<BestSigmaX>( v, c, v0x );
+    Base::template extract_param<BestSigmaY>( v, c, v0y );
     Base::template extract_param<DeltaSigmaX>( v, c, this->dzx );
     Base::template extract_param<DeltaSigmaY>( v, c, this->dzy );
     Base::template extract_param<ZAtBestSigmaX>( v, c, z0x );
@@ -24,8 +24,13 @@ bool ParameterHelper<Special>::prepare(
     Base::template extract_param<MeanZ>( v, c, z );
 
     zdx = (z - z0x).cwise() * this->dzx; zdy = (z - z0y).cwise() * this->dzy;
-    this->sx = (zdx.cwise().square() + v0x).cwise().sqrt();
-    this->sy = (zdy.cwise().square() + v0y).cwise().sqrt();
+    if ( Special::Use_Holtzer_PSF ) {
+        this->sx = (zdx.cwise().square() + v0x.cwise().square()).cwise().sqrt();
+        this->sy = (zdy.cwise().square() + v0y.cwise().square()).cwise().sqrt();
+    } else {
+        this->sx = zdx.cwise().square() + v0x;
+        this->sy = zdy.cwise().square() + v0y;
+    }
     zdx.cwise() *= dzx; zdy.cwise() *= dzy;
 
     if ( ! Base::check(x_low, y_low) )
