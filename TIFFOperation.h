d48262b7f00c7585a4fc6c8a1ec494c51db31b9c
stw78ye@BIOPHYSIK.LOCAL <stw78ye@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2010-07-15 09:10:51 +0000

    Cleaned up TIFF error handling. Added functionality for ignoring warnings in TIFF input.
    git-svn-id: file:///scratch/steve/svn/rapidSTORM/branches/rapid2storm-1.5/src@362 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

---
+++ TIFFOperation.h
@@ -0,0 +1,34 @@
+#include <dStorm/helpers/thread.h>
+#include <string>
+#include <list>
+#include <tiffio.h>
+#include <stdarg.h>
+#include <simparm/Message.hh>
+
+namespace dStorm {
+
+class TIFFOperation {
+    static ost::Mutex mutex;
+    static TIFFOperation* current;
+
+    ost::MutexLock lock;
+    simparm::Node& message_handler;
+    std::list<simparm::Message> errors;
+    const std::string error_title;
+
+    TIFFErrorHandler old_warning_handler;
+    TIFFErrorHandler old_error_handler;
+
+    static void make_warning(const char *module, const char *fmt, va_list ap);
+    static void make_error(const char *module, const char *fmt, va_list ap);
+    static void ignore(const char *module, const char *fmt, va_list ap);
+
+  public:
+    TIFFOperation( 
+        std::string error_title,
+        simparm::Node& message_handler,
+        bool suppress_warnings );
+    void throw_exception_for_errors();
+    ~TIFFOperation();
+};
+}
