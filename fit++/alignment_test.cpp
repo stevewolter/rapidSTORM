dac11019a7bfe5ea6cc838ff3cfb4c998941c3ad
stw78ye@BIOPHYSIK.LOCAL <stw78ye@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2011-03-04 21:50:32 +0000

    Fixed bugs, made debian-buildable.
    git-svn-id: file:///scratch/steve/svn/rapidSTORM-dev/rapidSTORM/trunk@1479 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

--- fit++/alignment_test.cpp
+++ fit++/alignment_test.cpp
@@ -1,20 +1,17 @@
-#include <dStorm/helpers/thread.h>
+#include <boost/thread/thread.hpp>
 #include <stdio.h>
 #include <Eigen/Core>
 #include <cassert>
 
-struct T : public ost::Thread {
-  T() : ost::Thread("Test") {}
-  void run() throw() {
+void run() {
     EIGEN_ALIGN_128 int bar;
     assert( /* Integer is properly aligned */ 
         ((int)(&bar) & (0xf)) == 0 );
-  }
-};
+}
 
 int main() {
-    T t;
-    t.start();
-    t.join();
+    boost::thread thread;
+    thread = boost::thread( &run );
+    thread.join();
     return 0;
 }
