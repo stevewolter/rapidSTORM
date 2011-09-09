1402a86b8bd241fd73bd59a59651881464a017f5
swolter@BIOPHYSIK.LOCAL <swolter@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2011-09-09 15:25:14 +0000

    Rewrote background noise estimator to estimate each plane individually
    git-svn-id: file:///scratch/steve/svn/rapidSTORM-dev/rapidSTORM/trunk@2635 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

--- inputs/BackgroundDeviationEstimator.cpp
+++ inputs/BackgroundDeviationEstimator.cpp
@@ -7,6 +7,7 @@
 #include <dStorm/input/chain/Context.h>
 #include <dStorm/input/chain/Filter_impl.h>
 #include <dStorm/ImageTraits.h>
+#include <dStorm/image/slice.h>
 #include <simparm/Message.hh>
 
 using namespace dStorm::engine;
@@ -46,13 +47,50 @@ Config::Config()
 {
 }
 
-static void add_to_histogram( const engine::Image& im, int h[], const int b ) 
+ImagePlane::ImagePlane( int binning ) 
+: binning(binning), highest_bin(1), total_counts(0)
 {
-    DEBUG("Got positions " << im.begin().position().transpose() << " to " << im.end().position().transpose() << ", which are " << ((im.begin() == im.end()) ? "equal" : "not equal") );
-    for ( engine::Image::const_iterator i = im.begin(), e = im.end(); i != e; ++i ) 
-    {
-        h[ *i >> b ] += 1;
+    int hist_size = (int(std::numeric_limits<engine::StormPixel>::max())+1) >> binning;
+    histogram.reset( new int[hist_size]);
+    for (int i = 0; i < hist_size; i++) histogram[i] = 0;
+
+}
+
+void ImagePlane::add_image( const Image<engine::StormPixel,2>& im )
+{
+    for ( Image<engine::StormPixel,2>::const_iterator i = im.begin(), e = im.end(); i != e; ++i ) {
+        if ( ++histogram[ *i >> binning ] > histogram[highest_bin] )
+            highest_bin = *i >> binning;
     }
+    total_counts += im.size_in_pixels();
+}
+
+bool ImagePlane::converged() const {
+    return ( total_counts >= 100000 );
+}
+
+camera_response ImagePlane::background_stddev() const
+{
+    if ( highest_bin == 0 ) {
+        throw lowest_histogram_mode_is_strongest();
+    } else {
+        double S = 0, sumweight = 0;
+        double mean = -1, variance = -1;
+
+        mean = variance = 0;
+        for (int i = 0; i <= highest_bin; ++i) {
+            if ( histogram[i] == 0 ) continue;
+            double newweight = sumweight + histogram[i];
+            double Q = ((i << binning) - mean), R = Q * histogram[i] / newweight;
+            S += sumweight * Q * R;
+            mean += R;
+            sumweight = newweight;
+            DEBUG("Mean changed to " << mean << " and variance to " << S * (highest_bin+1) / ( (highest_bin) * sumweight) );
+        }
+        variance = S * (highest_bin+1) / ( (highest_bin) * sumweight);
+        return float(sqrt(variance)) * camera::ad_count;
+    }
+
 }
 
 Source::Source(std::auto_ptr<input::Source<engine::Image> > base)
@@ -66,52 +104,32 @@ Source::get_traits()
 {
     DEBUG("Running background standard deviation estimation");
     Source::TraitsPtr s = base->get_traits();
-    int hist_size = (int(std::numeric_limits<engine::StormPixel>::max())+1) >> binning;
-    int histogram[hist_size];
-    for (int i = 0; i < hist_size; i++) histogram[i] = 0;
+    std::vector<ImagePlane> planes( s->plane_count(), ImagePlane(binning) );
 
-    int highest_bin = 1, histogram_size = 0;
-    double mean = -1, variance = -1;
     for (input::Source<engine::Image>::iterator i = base->begin(), e = base->end(); i != e; ++i ) {
         DEBUG("Loaded image " << i->frame_number());
         if ( i->is_invalid() ) continue;
         DEBUG("Got image of size " << i->width_in_pixels() << " " << i->height_in_pixels() << " " << i->depth_in_pixels());
         DEBUG("Adding image to histogram");
-        add_to_histogram( *i, histogram, binning );
         
-        highest_bin = 0;
-        for (int j = 1; j < hist_size; ++j)  {
-            //DEBUG("Image " << i->frame_number().value() << " bin " << (j << binning) << " " << histogram[j]);
-            if ( histogram[highest_bin] < histogram[j] )
-                highest_bin = j;
+        bool all_images_converged = true;
+        for (int j = 0; j < s->plane_count(); ++j) {
+            planes[j].add_image( i->slice(2, j * camera::pixel) );
+            all_images_converged = all_images_converged && planes[j].converged();
         }
-        histogram_size = 0;
-        for (int i = 0; i <= highest_bin; ++i) histogram_size += histogram[i];
-        DEBUG("Histogram has " << histogram_size << " pixels and highest bin is " << highest_bin);
-        if ( histogram_size >= 100000 ) break;
+        if ( all_images_converged ) break;
     }
 
-    if ( highest_bin == 0 ) {
-        simparm::Message m("Background standard deviation estimation failed",
-            "The most common pixel in this image is also the lowest. I cannot compute a background standard deviation, sorry. "
-            "Some things, like determining precision of localization, will not work.",
-            simparm::Message::Warning);
-        base->getNode().send(m);
-    } else {
-        double S = 0, sumweight = 0;
-        mean = variance = 0;
-        for (int i = 0; i <= highest_bin; ++i) {
-            if ( histogram[i] == 0 ) continue;
-            double newweight = sumweight + histogram[i];
-            double Q = ((i << binning) - mean), R = Q * histogram[i] / newweight;
-            S += sumweight * Q * R;
-            mean += R;
-            sumweight = newweight;
-            DEBUG("Mean changed to " << mean << " and variance to " << S * (highest_bin+1) / ( (highest_bin) * sumweight) );
+    for (int j = 0; j < s->plane_count(); ++j) 
+        try {
+            s->plane(j).background_stddev = planes[j].background_stddev();
+        } catch ( const lowest_histogram_mode_is_strongest& ) {
+            simparm::Message m("Background standard deviation estimation failed",
+                "The most common pixel in this image is also the lowest. I cannot compute a background standard deviation, sorry. "
+                "Some things, like determining precision of localization, will not work.",
+                simparm::Message::Warning);
+            base->getNode().send(m);
         }
-        variance = S * (highest_bin+1) / ( (highest_bin) * sumweight);
-        s->background_stddev = float(sqrt(variance)) * camera::ad_count;
-    }
 
     DEBUG("Dispatching restart message");
     base->dispatch( BaseSource::RepeatInput );
--- inputs/BackgroundDeviationEstimator.h
+++ inputs/BackgroundDeviationEstimator.h
@@ -9,6 +9,7 @@
 #include <dStorm/input/chain/Filter.h>
 #include <simparm/Entry.hh>
 #include <boost/mpl/vector.hpp>
+#include <boost/shared_array.hpp>
 
 namespace dStorm {
 namespace BackgroundStddevEstimator {
@@ -22,6 +23,22 @@ struct Config : public simparm::Object
     void registerNamedEntries() { push_back(disable); }
 };
 
+struct lowest_histogram_mode_is_strongest : public std::runtime_error {
+    lowest_histogram_mode_is_strongest() 
+        : std::runtime_error("The lowest histogram pixel in the background estimation is the strongest") {}
+};
+
+struct ImagePlane {
+    ImagePlane( int binning );
+    void add_image( const Image<engine::StormPixel,2>& );
+    bool converged() const;
+    camera_response background_stddev() const;
+  private:
+    const int binning;
+    int highest_bin, total_counts;
+    boost::shared_array<int> histogram;
+};
+
 class Source 
 : public input::Source<engine::Image>,
   public input::Filter,
@@ -29,6 +46,7 @@ class Source
 {
     std::auto_ptr< input::Source<engine::Image> > base;
     int confidence_limit, binning;
+
   public:
     Source(std::auto_ptr< input::Source<engine::Image> > base);
 
