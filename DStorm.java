3c286c2b4b86747a6e7cac4c2cb515c63a071368
swolter@BIOPHYSIK.LOCAL <swolter@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2011-10-21 17:17:53 +0000

    Fixed handling of paths in installer
    git-svn-id: file:///scratch/steve/svn/rapidSTORM-dev/rapidSTORM/trunk@3164 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

--- DStorm.java
+++ DStorm.java
@@ -34,7 +34,8 @@ class DStorm {
     }
 
    private static String[] build_environment(DefaultPath path) {
-        boolean set_magick_path = false;
+        boolean set_magick_path = new File( path.getPrefix(), 
+            "share" + File.separator + "GraphicsMagick-1.3.12" ).exists();
         Set<Map.Entry<String,String> > env = System.getenv().entrySet();
         String[] result_env = new String[ env.size() + ( (set_magick_path) ? 2 : 1 ) ];
         int i = 0;
@@ -79,17 +80,21 @@ class DStorm {
         String[] environment = build_environment( path );
         java.util.Vector<String> args = new java.util.Vector<String>();
         args.add(path.getExecutable().getPath() );
-        boolean gui = true;
+        boolean gui = true, load_config = true, verbose = false;
         for (int i = 0; i < cmdline_args.length; ++i) {
             if ( cmdline_args[i].equals("--StderrPipe") ) args.add("--StderrPipe");
             if ( cmdline_args[i].equals("--StdoutPipe") ) args.add("--StdoutPipe");
             if ( cmdline_args[i].equals("--no-gui") ) gui = false;
+            if ( cmdline_args[i].equals("--no-load-config") ) load_config = false;
+            if ( cmdline_args[i].equals("--verbose") ) verbose = true;
         }
-        args.add("--config");
-        args.add( path.getSystemConfigFile().getPath() );
-        if ( path.getUserConfigFile().exists() ) {
+        if ( load_config ) {
             args.add("--config");
-            args.add( path.getUserConfigFile().getPath() );
+            args.add( path.getSystemConfigFile().getPath() );
+            if ( path.getUserConfigFile().exists() ) {
+                args.add("--config");
+                args.add( path.getUserConfigFile().getPath() );
+            }
         }
         args.add("--Twiddler");
 
@@ -97,6 +102,12 @@ class DStorm {
         if ( gui ) {
             Main.StartWithEnv(to_exec, environment, title);
         } else {
+            if ( verbose ) {
+                for (int i = 0; i < to_exec.length; ++i)
+                    System.err.println("Arg " + i + " is " + to_exec[i]);
+                for (int i = 0; i < environment.length; ++i)
+                    System.err.println("Environment " + i + " is " + environment[i]);
+            }
             Process p = Runtime.getRuntime().exec( to_exec, environment );
             Thread in = new au.com.pulo.kev.simparm.PipingThread( System.in, new PrintStream(p.getOutputStream()) );
             Thread out = new au.com.pulo.kev.simparm.PipingThread( p.getInputStream(), System.out );
@@ -104,9 +115,10 @@ class DStorm {
             out.start();
             err.start();
             in.start();
-            p.waitFor();
+            int rv = p.waitFor();
             out.join();
             err.join();
+            System.out.println("RAPIDSTORM EXITED WITH CODE " + rv);
         }
      } catch (Exception e) {
         try {
