44bdc59cff9a862295e880d581a2309e1ef3acb5
swolter@BIOPHYSIK.LOCAL <swolter@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2012-02-25 18:45:12 +0000

    Fixed race conditions in job control
    git-svn-id: file:///scratch/steve/svn/rapidSTORM-dev/rapidSTORM/trunk@3903 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

---
+++ job/Control.cpp
@@ -0,0 +1,107 @@
+#include "debug.h"
+#include "Control.h"
+#include "Run.h"
+
+namespace dStorm {
+namespace job {
+
+Control::Control( bool auto_terminate )
+: simparm::Listener( simparm::Event::ValueChanged ),
+  close_job( auto_terminate ),
+  abort_job( false ),
+  abortJob("StopComputation", "Stop computation"),
+  closeJob("CloseJob", "Close job")
+{
+    closeJob.helpID = "#CloseJob";
+    abortJob.helpID = "#StopEngine";
+
+    receive_changes_from( abortJob.value );
+    receive_changes_from( closeJob.value );
+}
+
+void Control::registerNamedEntries( simparm::Node& runtime_config )
+{
+    runtime_config.push_back( abortJob );
+    runtime_config.push_back( closeJob );
+}
+
+void Control::wait_until_termination_is_allowed()
+{
+    boost::unique_lock<boost::mutex> lock( mutex );
+    while ( ! close_job )
+        allow_termination.wait(lock);
+}
+
+void Control::stop() {
+    boost::lock_guard<boost::mutex> lock( mutex );
+    closeJob.editable = false;
+    abortJob.editable = false;
+    close_job = true;
+    abort_job = true;
+    if ( current_run ) current_run->interrupt();
+    allow_termination.notify_all();
+}
+
+void Control::restart() {
+    boost::lock_guard<boost::mutex> lock( mutex );
+    assert( current_run );
+    current_run->restart();
+}
+
+void Control::repeat_results() {
+    throw std::logic_error("Cannot repeat results from dSTORM master job control");
+}
+bool Control::can_repeat_results() { return false; }
+
+void Control::change_input_traits( std::auto_ptr< input::BaseTraits > new_traits )
+{
+    next_run_traits = new_traits;
+}
+
+std::auto_ptr<EngineBlock> Control::block() {
+    assert( current_run );
+    return current_run->block();
+}
+
+void Control::set_current_run( boost::shared_ptr<Run> r ) {
+    boost::lock_guard<boost::mutex> lock( mutex );
+    current_run = r;
+    if ( r.get() && abort_job )
+        r->interrupt();
+}
+
+void Control::operator()(const simparm::Event& e) {
+    if ( &e.source == &closeJob.value && e.cause == simparm::Event::ValueChanged && closeJob.triggered() )
+    {
+        closeJob.untrigger();
+        DEBUG("Job close button allows termination" );
+        boost::lock_guard<boost::mutex> lock( mutex );
+        closeJob.editable = false;
+        close_job = true;
+        abort_job = true;
+        if ( current_run ) current_run->interrupt();
+        allow_termination.notify_all();
+    } else if ( &e.source == &abortJob.value && e.cause == simparm::Event::ValueChanged && abortJob.triggered() )
+    {
+        abortJob.untrigger();
+        DEBUG("Abort job button pressed");
+        boost::lock_guard<boost::mutex> lock( mutex );
+        abortJob.editable = false;
+        abort_job = true;
+        if ( current_run ) current_run->interrupt();
+    }
+}
+
+bool Control::aborted_by_user() const
+{
+    boost::lock_guard<boost::mutex> lock( mutex );
+    return abort_job;
+}
+
+bool Control::continue_computing() const {
+    boost::lock_guard<boost::mutex> lock( mutex );
+    return ! abort_job;
+}
+
+}
+}
---
+++ job/Control.h
@@ -0,0 +1,53 @@
+#ifndef DSTORM_JOB_USERCONTROL_H
+#define DSTORM_JOB_USERCONTROL_H
+
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+#include <dStorm/Engine.h>
+#include <simparm/TriggerEntry.hh>
+#include <simparm/Callback.hh>
+
+namespace dStorm {
+namespace job {
+
+class Run;
+
+class Control 
+: private simparm::Node::Callback ,
+  public dStorm::Engine
+{
+    mutable boost::mutex mutex;
+    bool close_job, abort_job;
+    simparm::TriggerEntry abortJob;
+    simparm::TriggerEntry closeJob;
+    boost::condition allow_termination;
+
+    boost::shared_ptr<Run> current_run;
+    std::auto_ptr< input::BaseTraits > next_run_traits;
+
+    /** Receive the signal from closeJob. */
+    void operator()(const simparm::Event&);
+
+public:
+    Control( bool auto_terminate );
+    void registerNamedEntries( simparm::Node& );
+    void set_current_run( boost::shared_ptr<Run> );
+    void wait_until_termination_is_allowed();
+    void stop();
+
+    void restart();
+    void repeat_results();
+    bool can_repeat_results();
+    void change_input_traits( std::auto_ptr< input::BaseTraits > );
+    std::auto_ptr<EngineBlock> block();
+
+    bool aborted_by_user() const;
+    bool continue_computing() const;
+    bool traits_changed() const { return next_run_traits.get(); }
+    std::auto_ptr< input::BaseTraits > new_traits() { return next_run_traits; }
+};
+
+}
+}
+
+#endif
