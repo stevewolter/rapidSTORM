fa674e3dee7f17e0fc60c82e0f7028a320b01d28
swolter@BIOPHYSIK.LOCAL <swolter@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2012-04-25 16:08:45 +0000

    Added looked-over file
    git-svn-id: file:///scratch/steve/svn/rapidSTORM-dev/rapidSTORM/trunk@4289 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

---
+++ fit_window/Stack.hpp
@@ -0,0 +1,40 @@
+#ifndef DSTORM_GUF_DATACUBE_HPP
+#define DSTORM_GUF_DATACUBE_HPP
+
+#include "Stack.h"
+#include "Optics.h"
+#include "PlaneCreator.hpp"
+#include "ScheduleIndexFinder.hpp"
+#include <dStorm/engine/InputTraits.h>
+
+namespace dStorm {
+namespace fit_window {
+
+class StackCreator::Plane {
+private:
+    Optics optics;
+    ScheduleIndexFinder index_finder;
+    PlaneCreatorTable extractor_table;
+
+public:
+    template <typename Schedule>
+    Plane( const Config& c, const engine::InputPlane& plane, Schedule s, int max_width )
+        : optics( Spot::Constant( Spot::Scalar( c.fit_window_size() ) ), plane ),
+          index_finder( s, c.allow_disjoint(), c.double_computation(), optics, max_width ),
+          extractor_table( s, optics )
+        {}
+    std::auto_ptr<fit_window::Plane> set_image( const engine::Image2D& image, const Spot& position ) const; 
+};
+
+template <typename Schedule>
+StackCreator::StackCreator( const Config& ad, const dStorm::engine::JobInfo& info, Schedule s, int max_width )
+{
+    for (int i = 0; i < info.traits.plane_count(); ++i)
+        planes.push_back( new Plane( ad, info.traits.plane(i), s, max_width ) );
+}
+
+
+}
+}
+
+#endif
