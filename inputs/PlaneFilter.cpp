57eab4b0cc1582da5c98551028e4e54e6718c699
swolter@BIOPHYSIK.LOCAL <swolter@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2012-05-15 18:31:53 +0000

    Fixed integration tests for simparm output
    git-svn-id: file:///scratch/steve/svn/rapidSTORM-dev/rapidSTORM/trunk@4510 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

--- inputs/PlaneFilter.cpp
+++ inputs/PlaneFilter.cpp
@@ -48,6 +48,7 @@ struct AllPlanes : public PlaneSelection {
     AllPlanes* clone() const { return new AllPlanes(*this); }
     bool selects_plane() const { return false; }
     int plane_index() const { throw std::logic_error("No plane selectable"); }
+    void attach_ui( simparm::Node& at ) { attach_parent(at); }
 };
 
 struct SinglePlane : public PlaneSelection {
@@ -60,6 +61,7 @@ struct SinglePlane : public PlaneSelection {
 
     bool selects_plane() const { return true; }
     int plane_index() const { return index; }
+    void attach_ui( simparm::Node& at ) { attach_parent(at); }
 };
 
 struct Config : public simparm::Object {
--- simparm/test_eigen.cpp
+++ simparm/test_eigen.cpp
@@ -32,32 +32,12 @@ namespace Eigen {
 using namespace std;
 using namespace simparm;
 
-static simparm::Entry< Eigen::Matrix<float, 2,2, Eigen::DontAlign> > float_matrix("FloatMatrix", "FloatMatrix");
-static 
+int main() {
+    simparm::Entry< Eigen::Matrix<float, 2,2, Eigen::DontAlign> > float_matrix("FloatMatrix", "FloatMatrix");
     simparm::Entry< Eigen::Matrix< boost::units::quantity<boost::units::si::length,double>, 2,1, Eigen::DontAlign> > 
         meter_vector("MeterVector", "MeterVector");
-static
     simparm::Entry< boost::units::quantity<boost::units::si::length,unsigned int> > bounded_entry("BoundedEntry", "Bounded entry");
 
-struct ChangeShower : public Listener {
-    ChangeShower() {
-        receive_changes_from( float_matrix.value );
-        receive_changes_from( bounded_entry.value );
-    }
-    void operator()( const simparm::Event& e ) {
-        if ( e.cause == Event::ValueChanged ) {
-            if ( &e.source == &float_matrix.value ) {
-                std::cerr <<  "Float matrix changed to " << float_matrix().row(0) << " and " << float_matrix().row(1) << std::endl;
-            } else if ( &e.source == &bounded_entry.value ) {
-                sleep(1);
-                std::cerr << "Finished change to " << bounded_entry().value() << std::endl;
-            }
-        }
-    }
-};
-
-int main() {
-    ChangeShower c;
     float_matrix = Eigen::Matrix<float, 2,2, Eigen::DontAlign>::Zero();
     simparm::Entry< boost::units::quantity<boost::units::si::length,double> > normal_entry("NormalEntry", "Normal entry");
     bounded_entry.value = 15 * boost::units::si::meter;
