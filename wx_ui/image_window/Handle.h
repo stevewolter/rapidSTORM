cd6944f65061435b47c4c2887eb615eca2b0913c
swolter@BIOPHYSIK.LOCAL <swolter@BIOPHYSIK.LOCAL@6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5>
2012-06-08 12:59:36 +0000

    More work on wxWidgets integration
    git-svn-id: file:///scratch/steve/svn/rapidSTORM-dev/rapidSTORM/trunk@4667 6dd997e4-92ef-4477-b1f0-deb9b3b8d6d5

---
+++ wx_ui/image_window/Handle.h
@@ -0,0 +1,26 @@
+#ifndef SIMPARM_WX_UI_IMAGE_WINDOW_HANDLE_H
+#define SIMPARM_WX_UI_IMAGE_WINDOW_HANDLE_H
+
+#include <dStorm/display/Manager.h>
+
+namespace simparm {
+namespace wx_ui {
+namespace image_window {
+
+struct Handle
+: public dStorm::display::WindowHandle
+{
+    boost::shared_ptr< SharedDataSource > data_source;
+    boost::shared_ptr< Window* > associated_window;
+
+    Handle( dStorm::display::DataSource& );
+    ~Handle();
+
+    void store_current_display( dStorm::display::SaveRequest );
+};
+
+}
+}
+}
+
+#endif
