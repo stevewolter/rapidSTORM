be1a3005d7569a4a82541c3c8b701fd6a1349552
Steve Wolter <swolter@sdf.org>
2014-02-05 19:08:40 +0100

    Fixed range propagation in Kalman filter

--- kalman_filter/EmissionTracker.cpp
+++ kalman_filter/EmissionTracker.cpp
@@ -199,8 +199,8 @@ Output::announceStormSize(const Announcement &a)
     static_cast< dStorm::input::Traits<Localization>& >(my_announcement) 
         = dStorm::input::Traits<Localization>();
     my_announcement.in_sequence = true;
-    my_announcement.position_x().is_given = true;
-    my_announcement.position_y().is_given = true;
+    my_announcement.position_x() = a.position_x();
+    my_announcement.position_y() = a.position_y();
     my_announcement.amplitude() = a.amplitude();
     my_announcement.image_number() = a.image_number();
     my_announcement.source_traits.push_back( 
