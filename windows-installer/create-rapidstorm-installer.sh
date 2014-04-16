5a5ff83824004802be8e076bdc8cb83a26542704
Steve Wolter <swolter@sdf.org>
2014-04-16 22:37:15 +0200

    Fixed windows installer creation script to explicitly download the right mingw DLLs instead of reusing host files

--- windows-installer/create-rapidstorm-installer.sh
+++ windows-installer/create-rapidstorm-installer.sh
@@ -13,7 +13,7 @@ declare -r VERSION=$3
 declare -r MARKETING_VERSION="$1.$2"
 declare -r MAJOR_VERSION="$1"
 
-while read ARCH HOST_TYPE MARKETING_ARCH; do
+while read ARCH HOST_TYPE MARKETING_ARCH COMPILER_PACKAGE; do
     TEMPDIR="tmp-${ARCH}"
     APT_DIR="$(pwd)/${TEMPDIR}/apt-tree"
     STAGE_DIR="${TEMPDIR}/stage"
@@ -23,7 +23,7 @@ while read ARCH HOST_TYPE MARKETING_ARCH; do
 
     cat > ${APT_CONFIG} <<EOF
 APT::Architecture "${ARCH}";
-APT::Architectures { "mingw-i686"; "mingw-amd64"; }
+APT::Architectures { "mingw-i686"; "mingw-amd64"; "amd64"; }
 Dir::State      "${APT_DIR}/var/lib/apt";
 Dir::Cache      "${APT_DIR}/var/cache/apt";
 Dir::Etc        "${APT_DIR}/etc/apt";
@@ -35,7 +35,9 @@ EOF
 
     mkdir --parents ${APT_DIR}/etc/apt/preferences.d ${APT_DIR}/etc/apt/apt.conf.d
     cat > ${APT_DIR}/etc/apt/sources.list <<EOF
-deb ${REPOSITORY} ${DIST} main non-free contrib mingw
+deb [arch=mingw-i686,mingw-amd64] ${REPOSITORY} ${DIST} main non-free contrib mingw
+# We need the main Debian repository for the gcc-mingw-w64-* package
+deb [arch=amd64] http://ftp.de.debian.org/debian/ ${DIST} main
 EOF
 
     mkdir --parents ${APT_DIR}/var/{cache/apt/archives,lib/{apt/{lists,mirrors},dpkg}}
@@ -44,7 +46,18 @@ EOF
 
     APT_CONFIG=${APT_CONFIG} apt-get --download-only --allow-unauthenticated install rapidstorm=${VERSION} rapidstorm-doc=${VERSION}
 
+    pushd ${TEMPDIR}
+    APT_CONFIG=${APT_CONFIG} apt-get download --allow-unauthenticated ${COMPILER_PACKAGE}:amd64
+    popd
+
     mkdir -p "${STAGE_DIR}"
+
+    # We need only the dll's that are packaged into the gcc compiler.
+    # Extract them first and delete the rest.
+    dpkg -x ${TEMPDIR}/${COMPILER_PACKAGE}_*_amd64.deb ${STAGE_DIR}
+    find ${STAGE_DIR} -type f -not -name '*.dll' -delete
+
+    # Now extract all the rest over it.
     find "${APT_DIR}/var/cache/apt" -name '*.deb' -exec dpkg -x '{}' ${STAGE_DIR} ';'
     find ${STAGE_DIR} -iname '*.exe' -or -iname '*.dll' -exec ${HOST_TYPE}-strip '{}' ';'
     find ${STAGE_DIR}/{bin,usr/lib} -iname '*.dll' -exec mv -n '{}' ${STAGE_DIR}/usr/bin ';'
@@ -60,7 +73,6 @@ EOF
         cat $(dirname $0)/${ARCH}.iss
 
         echo "[Files]"; echo;
-        find /usr/lib/gcc/${HOST_TYPE} -name '*.dll' -printf "Source: \"Z:%p\"; DestDir: \"{app}/bin\"\n"
         pushd ${STAGE_DIR}/usr >/dev/null
         find -type f -printf "Source: \"Z:$(pwd)/%p\"; DestDir: \"{app}/%h/\"; Flags: ignoreversion\n" | sed -e 's:/\./:/:g'
         popd >/dev/null
@@ -70,6 +82,6 @@ EOF
     mv ${TEMPDIR}/Output/setup.exe rapidstorm-${MARKETING_VERSION}-${MARKETING_ARCH}.exe
     rm -rf ${TEMPDIR}
 done <<EOF
-mingw-i686 i686-w64-mingw32 win32
-mingw-amd64 x86_64-w64-mingw32 win64
+mingw-i686 i686-w64-mingw32 win32 gcc-mingw-w64-i686
+mingw-amd64 x86_64-w64-mingw32 win64 gcc-mingw-w64-x86-64
 EOF
