0cd663f921eabf08e94a45cc5b0dbb25266fd40f
Steve Wolter <rapidstorm@swolter.sdf1.org>
2014-04-27 22:10:25 +0200

    Removed unused source file DisjointData.cpp

--- nonlinfit/unit_test.cpp
+++ nonlinfit/unit_test.cpp
@@ -4,13 +4,11 @@ namespace nonlinfit {
 
 namespace sum { boost::unit_test::test_suite* register_unit_tests(); }
 namespace levmar { boost::unit_test::test_suite* register_unit_tests(); }
-namespace plane { boost::unit_test::test_suite* register_unit_tests(); }
 
 boost::unit_test::test_suite* register_unit_tests() {
     boost::unit_test::test_suite* rv = BOOST_TEST_SUITE( "nonlinfit" );
     rv->add( sum::register_unit_tests() );
     rv->add( levmar::register_unit_tests() );
-    rv->add( plane::register_unit_tests() );
     return rv;
 }
 
