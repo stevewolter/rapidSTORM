acc8cf46817e0f9855caa4ef00f673278a4b4cb3
Steve Wolter <swolter@sdf.org>
2014-07-03 00:17:00 +0200

    Moved the Run button to the bottom of the main window outside the scrolling area

--- simparm/wx_ui/WindowNode.h
+++ simparm/wx_ui/WindowNode.h
@@ -12,7 +12,9 @@ class WindowNode : public InnerNode {
     Sizer sizer;
     WindowSpecification window;
     virtual boost::shared_ptr<Window> create_window();
-public:
+  protected:
+    void set_self_growing() { window.proportion = 1; }
+  public:
     WindowNode( boost::shared_ptr<Node> n, std::string name ) ;
     virtual void set_description( std::string d ) { window.name = d; }
     void initialization_finished();
@@ -22,6 +24,15 @@ public:
     void add_full_width_line( WindowSpecification& w ) { sizer.add_full_width_line(w); }
     void add_full_width_sizer( SizerSpecification& w ) { sizer.add_full_width_sizer(w); }
     boost::shared_ptr< Window > get_parent_window() { return window.window; }
+};
+
+class TopWindowNode : public WindowNode {
+    boost::shared_ptr<bool> needs_fit_inside;
+
+  public:
+    TopWindowNode( boost::shared_ptr<Node> n, std::string name )
+        : WindowNode(n, name), needs_fit_inside(new bool(false)) {}
+    boost::function0<void> get_relayout_function();
 };
 
 }
