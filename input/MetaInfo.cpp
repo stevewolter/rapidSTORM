d16ad8a547ad9eed6b6f04ea9ffdad25a77c267a
Steve Wolter <swolter@sdf.org>
2014-07-07 22:19:34 +0200

    Removed unused signal ResolutionSetter

--- input/MetaInfo.cpp
+++ input/MetaInfo.cpp
@@ -1,6 +1,5 @@
 #include "input/MetaInfo.h"
 #include "signals/InputFileNameChange.h"
-#include "signals/ResolutionChange.h"
 #include "signals/BasenameChange.h"
 #include <boost/ptr_container/ptr_vector.hpp>
 
@@ -11,7 +10,6 @@ using namespace signals;
 
 struct MetaInfo::Signals
 : public InputFileNameChange,
-  public ResolutionChange,
   public BasenameChange
 {
     boost::ptr_vector< boost::signals2::scoped_connection > listeners;
@@ -38,7 +36,6 @@ template <typename Type>
 Type& MetaInfo::get_signal() { return *_signals; }
 
 template InputFileNameChange& MetaInfo::get_signal<InputFileNameChange>();
-template ResolutionChange& MetaInfo::get_signal<ResolutionChange>();
 template BasenameChange& MetaInfo::get_signal<BasenameChange>();
 
 void MetaInfo::forward_connections( const MetaInfo& s )
@@ -46,9 +43,6 @@ void MetaInfo::forward_connections( const MetaInfo& s )
     s._signals->listeners.push_back( 
         new boost::signals2::scoped_connection( 
             get_signal<InputFileNameChange>().connect( static_cast<InputFileNameChange&>(*s._signals) ) ) );
-    s._signals->listeners.push_back( 
-        new boost::signals2::scoped_connection( 
-            get_signal<ResolutionChange>().connect( static_cast<ResolutionChange&>(*s._signals) ) ) );
     s._signals->listeners.push_back( 
         new boost::signals2::scoped_connection( 
             get_signal<BasenameChange>().connect( static_cast<BasenameChange&>(*s._signals) ) ) );
