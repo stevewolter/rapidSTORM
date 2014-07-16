0a39de84af6e7172b351dcc7a4e87a3e4fbdc1e1
Steve Wolter <swolter@sdf.org>
2014-07-16 23:58:43 +0200

    Simplified InputBase class

--- inputs/InputBase.cpp
+++ inputs/InputBase.cpp
@@ -9,18 +9,16 @@ struct InputChainBase
 {
     simparm::Group input_config;
     InputChainBase() : input_config("Input") {}
-    ~InputChainBase() {}
 
-    InputChainBase* clone() const { return new InputChainBase(*this); }
-    std::string name() const { return input_config.getName(); }
-    std::string description() const { return input_config.getDesc(); }
-    void registerNamedEntries( simparm::NodeHandle node ) {
+    InputChainBase* clone() const OVERRIDE { return new InputChainBase(*this); }
+    std::string name() const OVERRIDE { return input_config.getName(); }
+    void registerNamedEntries( simparm::NodeHandle node ) OVERRIDE {
         Forwarder::registerNamedEntries( input_config.attach_ui(node) );
     }
 };
 
-std::auto_ptr< input::Link > make_input_base() {
-    return std::auto_ptr< input::Link >( new InputChainBase() );
+std::unique_ptr< input::Link > make_input_base() {
+    return make_unique<InputChainBase>();
 }
 
 }
